c7e3e379e934471207143238edf712537a63d787
Minh Quan Do <mdo6180@gmail.com>
2024-08-01 20:29:50 -0400

    got lsp-sample to work

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,23 +1,81 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as path from 'path';
+import { workspace, commands, window, ExtensionContext } from 'vscode';
+
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld-sample" is now active!');
+export function activate(context: ExtensionContext) {
+	/*
+	// The server is implemented in node
+	let serverModule = context.asAbsolutePath(path.join('server', 'server.py'));
 
+	const serverOptions: ServerOptions = {
+		command: 'python', // Replace with your own command.
+		args: [serverModule],
+	};
+	*/
+
+	// The server is implemented in node
+	const serverModule = context.asAbsolutePath(
+		path.join('server', 'out', 'server.js')
+	);
+
+	// If the extension is launched in debug mode then the debug server options are used
+	// Otherwise the run options are used
+	const serverOptions: ServerOptions = {
+		run: { module: serverModule, transport: TransportKind.ipc },
+		debug: {
+			module: serverModule,
+			transport: TransportKind.ipc,
+		}
+	};
+
+	let clientOptions: LanguageClientOptions = {
+        documentSelector: [{ scheme: 'file', language: 'plaintext' }],
+        synchronize: {
+            fileEvents: workspace.createFileSystemWatcher('**/.clientrc'),
+        },
+    };
+
+	client = new LanguageClient(
+        'exampleLanguageServer',
+        'Example Language Server',
+        serverOptions,
+        clientOptions
+    );
+
+    client.start();
+	
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	const disposable = commands.registerCommand('extension.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		window.showInformationMessage('Hello World!');
 	});
 
 	context.subscriptions.push(disposable);
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "helloworld-sample" is now active!');
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
 }
